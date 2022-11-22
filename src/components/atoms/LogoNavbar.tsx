d0779dd68f40feaf5b800bfe16f7454f003b5fd9
Gabriel <flaviogabrielsr0507@gmail.com>
2022-11-22 15:15:27 -0300

    update logo navbar

--- src/components/atoms/LogoNavbar.tsx
+++ src/components/atoms/LogoNavbar.tsx
@@ -12,7 +12,7 @@ export const LogoNavabar = (props: buttonProps) => {
 
   return (
     <>
-      <div className='hidden dark:flex items-center justify-between w-full text-lg p-1.5 border border-zinc-200 border-opacity-10 bg-zinc-100 bg-opacity-10 rounded-full '>
+      <div className='hidden dark:flex items-center justify-between  text-lg p-1.5 border border-zinc-200 border-opacity-10 bg-zinc-100 bg-opacity-10 rounded-full '>
         <Image 
           src={LogoWhite}
           alt='Logo'
@@ -21,7 +21,7 @@ export const LogoNavabar = (props: buttonProps) => {
           priority
         />      
       </div>
-      <div className='flex dark:hidden items-center justify-between w-full text-lg p-1.5 border border-zinc-200 border-opacity-10 bg-zinc-100 bg-opacity-10 rounded-full hover:scale-110 transition-all duration-200 '>
+      <div className='flex dark:hidden items-center justify-between  text-lg p-1.5 border border-zinc-200 border-opacity-10 bg-zinc-100 bg-opacity-10 rounded-full hover:scale-110 transition-all duration-200 '>
         <Image 
           src={Logo}
           alt='Logo'
